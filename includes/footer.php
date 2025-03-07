bc63a2242a34d7576b5df2a76ef6143d5ef8e774
Lucas Da Silva Ferreira <lucas-dasilvaferreira@hotmail.com>
2025-03-07 14:54:47 +0100

    fix (style.css): Correction des contrastes errors sur les badges de compétences;

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
   <!-- FOOTER -->
   <footer class="bg-dark text-white text-center py-3">
-    <p class="mb-0">© 2025 Lucas Da Silva Ferreira. Tous droits réservés.</p>
+    <p class="mb-0">© 2025 Lucas Da Silva Ferreira.</p>
   </footer>
 
   <!-- Bootstrap Bundle JS -->
